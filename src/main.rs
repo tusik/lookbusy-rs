e4b3a5aec479d2c158a50deebf9e8dea550b85bb
Image <image@by.cx>
2022-12-05 22:49:09 +0800

    first runable version

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
+/*
+ * @Author: Image image@by.cx
+ * @Date: 2022-12-05 21:40:45
+ * @LastEditors: Image image@by.cx
+ * @LastEditTime: 2022-12-05 22:35:29
+ * @FilePath: /lookbusy-rs/src/main.rs
+ * @Description: 
+ * 
+ * Copyright (c) 2022 by Image image@by.cx, All Rights Reserved. 
+ */
 use std::thread::{self, JoinHandle};
+use ctrlc;
 use clap::Parser;
+use rand::Rng;
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 pub struct Args{
@@ -11,11 +23,10 @@ pub struct Args{
 static mut HANDLES:Vec<JoinHandle<bool>> = vec![];
 static mut EAT_MEM:Vec<u64> = vec![];
 fn cpu_busy(cpu_num:u64){
-    let c = 14;
     for _i in 0..cpu_num{
-        let handle = thread::spawn(move ||{
+        let handle = thread::spawn(|| {
             loop{
-                _ = c*11;
+                _ = 2 * 11;
             };
         });
         unsafe{
@@ -25,15 +36,26 @@ fn cpu_busy(cpu_num:u64){
 }
 fn mem_busy(size_mb:u64){
     let target_size_bit = size_mb *1024 *1024 *8 /64;
-    for _i in 0.. target_size_bit{
+    for _i in 0..target_size_bit{
         unsafe{
-            EAT_MEM.push(1);
+            EAT_MEM.push(rand::thread_rng().gen());
         }
         
     }
 }
+fn print_info(args:&Args){
+    println!("Process start.");
+    println!("Now I'm eat {:} cpu and {:} MB Memory.",
+        args.cpu_num, args.mem_size);
+    println!("Use Ctrl + C to stop.");
+}
 fn main() {
     let args = Args::parse();
+    print_info(&args);
+    ctrlc::set_handler(||{
+        println!("\nTask Finished! bye~");
+        std::process::exit(0);        
+    }).expect("Error setting Ctrl-C handler");
     mem_busy(args.mem_size);
     cpu_busy(args.cpu_num);
     unsafe{
