74a3872423c23630f0cc89c599e68e4ab4cc70eb
Image <image@by.cx>
2022-12-07 22:21:33 +0800

    controll cpu usage

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 /*
  * @Author: Image image@by.cx
  * @Date: 2022-12-05 21:40:45
- * @LastEditors: Image_woker_pc image@by.cx
- * @LastEditTime: 2022-12-07 10:50:01
+ * @LastEditors: Image image@by.cx
+ * @LastEditTime: 2022-12-07 22:19:35
  * @FilePath: /lookbusy-rs/src/main.rs
  * @Description: 
  * 
@@ -16,20 +16,25 @@ use rand::Rng;
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 pub struct Args{
+    /// how many cpu thread you want use.
     #[clap(short, long, default_value_t=1)]
     cpu_num: u64,
+    /// cpu usage per thread %.
+    #[clap(short, long, default_value_t=1.0)]
+    limit: f32,
+    /// how many MB you want use.
     #[clap(short, long, default_value_t=1024)]
     mem_size: u64,
 }
 static mut HANDLES:Vec<JoinHandle<bool>> = vec![];
 static mut EAT_MEM:Vec<u64> = vec![];
-fn cpu_busy(cpu_num:u64){
+fn cpu_busy(cpu_num:u64, limit:f32){
     for _i in 0..cpu_num{
-        let handle = thread::spawn(|| {
+        let handle = thread::spawn(move || {
             loop{
                 let durations = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap();
-                if durations.as_millis() % 1000 > 500{
-                    thread::sleep(Duration::from_millis(500));
+                if durations.as_millis() % 1000 > (limit * 1000.0) as u128{
+                    thread::sleep(Duration::from_millis(10));
                 }
                 _ = 2 * 11;
             };
@@ -62,7 +67,7 @@ fn main() {
         std::process::exit(0);        
     }).expect("Error setting Ctrl-C handler");
     mem_busy(args.mem_size);
-    cpu_busy(args.cpu_num);
+    cpu_busy(args.cpu_num,args.limit);
     unsafe{
         for i in HANDLES.pop(){
             i.join().unwrap();
