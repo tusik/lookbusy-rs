9681054893b22f1d143147005d1f051d79bf2d9a
image <image@by.cx>
2024-04-17 21:04:33 +0800

    split worker

--- src/configure.rs
+++ src/configure.rs
@@ -1,10 +1,24 @@
-#[derive(Default)]
+use std::fs;
+use serde_derive::{Deserialize, Serialize};
+use toml::de::Error;
+#[derive(Serialize,Deserialize)]
+pub struct Config{
+    pub cpu: Vec<CPU>,
+}
+impl Config{
+    pub fn load(file:String) -> Result<Config, Error> {
+        let contents = fs::read_to_string(file).expect("Failed to read config file");
+        toml::from_str(&contents)
+    }
+}
+#[derive(Serialize,Deserialize)]
 pub struct CPU{
     // core index number
     pub core_id: u32,
     // cpu usage by %
     pub limit: f32,
     pub jitter: f32,
+    pub time:u64
 }
 impl CPU{
     pub fn from_config(config_str:&str)->CPU{
@@ -17,6 +31,7 @@ impl CPU{
             core_id: core,
             limit,
             jitter,
+            time:1
         }
     }
 }
